1904cbce31ddf706cd1e9e47588c9a2b33c6cda3
xmclallen <xmclallen@gmail.com>
2018-11-10 14:21:13 -0700

    Responsively display 6 posts in the layout order
    Using Bootstrap, in a rough mock-up:
     * On small screens, each item is listed as its own, 100% width of page
     * On medium screens, 2 rows of 3 equally spaced items
     * On large screens 1 row of 1 large item and 2 small,
       * 2nd row not completed.
    
    TODO: Create different 'view' types which will set width, layout of
     image/text/etc.

--- assessment1/src/App.js
+++ assessment1/src/App.js
@@ -8,8 +8,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="post_container">
-          <Post />
+        <header>blank</header>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-lg-6"><Post /></div>
+            <div class="col-md-4 col-lg-3"><Post /></div>
+            <div class="col-md-4 col-lg-3"><Post /></div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"><Post /></div>
+            <div class="col-md-4"><Post /></div>
+            <div class="col-md-4"><Post /></div>
+          </div>
         </div>
       </div>
     );
--- assessment1/src/Post.js
+++ assessment1/src/Post.js
@@ -5,9 +5,9 @@ class Post extends Component {
         return (
             <React.Fragment>
                 <div className="post-image">
-                <img src="http://via.placeholder.com/500"></img>
+                <img className="w-100 h-50" src="http://via.placeholder.com/500"></img>
                 </div>
-                <div className="post-body">
+                <div className="h-50 post-body">
                     <p><b>Lorem Ipsum Dolor Sit Amet</b></p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Teneo, inquit, finem illi videri nihil dolere. <i>Recte, inquit, intellegis.</i> Non semper, inquam; <a href="http://loripsum.net/" target="_blank">Et ille ridens: Video, inquit, quid agas;</a> Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Sed vos squalidius, illorum vides quam niteat oratio. <a href="http://loripsum.net/" target="_blank">Duo Reges: constructio interrete.</a> </p>
                 </div>
