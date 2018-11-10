901ddc1f8918e32f53ad7a3c96663378f36ae431
xmclallen <xmclallen@gmail.com>
2018-11-10 12:39:55 -0700

    Implement a "Post" component
    A static "post" is created with image, title, and description

--- assessment1/src/App.js
+++ assessment1/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Post from './Post.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        "Hello World"
+        <div className="post_container">
+          <Post />
+        </div>
       </div>
     );
   }
---
+++ assessment1/src/Post.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="post-image">
+                <img src="http://via.placeholder.com/500"></img>
+                </div>
+                <div className="post-body">
+                    <p><b>Lorem Ipsum Dolor Sit Amet</b></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Teneo, inquit, finem illi videri nihil dolere. <i>Recte, inquit, intellegis.</i> Non semper, inquam; <a href="http://loripsum.net/" target="_blank">Et ille ridens: Video, inquit, quid agas;</a> Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Sed vos squalidius, illorum vides quam niteat oratio. <a href="http://loripsum.net/" target="_blank">Duo Reges: constructio interrete.</a> </p>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+  
+export default Post;
+  
