f303c92f64980f4b42fd4161817b35e5e0cc2984
xmclallen <xmclallen@gmail.com>
2018-11-10 17:33:45 -0700

    Render posts via a variable component
    use the `let` keyword to define a variable which, and display that
     variable as the contents of the posts on the site.
    let should be able to be defined based on looping though the array of
     input props.

--- assessment1/src/PostsView.js
+++ assessment1/src/PostsView.js
@@ -4,17 +4,27 @@ import Post from './Post.js';
 
 class PostsView extends Component {
   render() {
+
+
+    let all_posts = <React.Fragment>
+    {/* GOAL is to define this variable programatically based on the props of render()
+         But for now, will settle with another mock-up
+          */}
+    <div class="col-md-4 col-lg-6"><Post data={this.props.posts[0]}/></div>
+    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[1]}/></div>
+    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[2]}/></div>
+    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[3]}/></div>
+    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[4]}/></div>
+    <div class="col-md-4 col-lg-6"><Post data={this.props.posts[5]}/></div>
+
+    </React.Fragment>
+    ;
+
+
     return (
       <React.Fragment>
         <div class="row">
-          <div class="col-md-4 col-lg-6"><Post data={this.props.posts[0]}/></div>
-          <div class="col-md-4 col-lg-3"><Post data={this.props.posts[1]}/></div>
-          <div class="col-md-4 col-lg-3"><Post data={this.props.posts[2]}/></div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 col-lg-3"><Post data={this.props.posts[3]}/></div>
-          <div class="col-md-4 col-lg-3"><Post data={this.props.posts[4]}/></div>
-          <div class="col-md-4 col-lg-6"><Post data={this.props.posts[5]}/></div>
+          {all_posts}
         </div>
       </React.Fragment>
     );
