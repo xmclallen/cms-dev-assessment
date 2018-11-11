2802fe150cec242d442f5417e96f805aaa2ca927
xmclallen <xmclallen@gmail.com>
2018-11-11 16:48:14 -0700

    Created event handlers for the sort buttons

--- assessment3/src/SortHeader.js
+++ assessment3/src/SortHeader.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 
 class SortHeader extends Component {
+    
+    
+    buttonClicked(str){
+        alert(str);
+    }
+
     render(){
-
-
         return(
             <div class="sort-header row">
-            <div class="col-sm-1" ><button class="btn btn-primary btn-md active">All</button></div>
-            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active">Listings</button></div>
-            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active" role="button">Events</button></div>
-            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active" role="button">Offers</button></div>
+            <div class="col-sm-1" ><button class="btn btn-primary btn-md active" onClick={this.buttonClicked.bind(this, "all")} >All</button></div>
+            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active" onClick={this.buttonClicked.bind(this, "Listings")} >Listings</button></div>
+            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active" onClick={this.buttonClicked.bind(this, "Events")} >Events</button></div>
+            <div class="col-sm-1" ><button class="btn btn-secondary btn-md active" onClick={this.buttonClicked.bind(this, "Offers")} >Offers</button></div>
           </div>
         );
     }
