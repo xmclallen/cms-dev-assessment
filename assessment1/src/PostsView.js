e472ac7013c2a5ad3259ab7f2c6c8d827c6af165
xmclallen <xmclallen@gmail.com>
2018-11-10 17:57:13 -0700

    Programatically build the post comps via input[]
    Loop through the input array with a for-loop and
     put the proper Post item in an array
    
    Display that second array
    
    NOTE: the map function could likely be used as well, went
    with a for loop because it is simple, well understood

--- assessment1/src/PostsView.js
+++ assessment1/src/PostsView.js
@@ -4,27 +4,32 @@ import Post from './Post.js';
 
 class PostsView extends Component {
   render() {
+    let p_items=this.props.posts;
+    let display_posts = [];
 
-
-    let all_posts = <React.Fragment>
-    {/* GOAL is to define this variable programatically based on the props of render()
-         But for now, will settle with another mock-up
-          */}
-    <div class="col-md-4 col-lg-6"><Post data={this.props.posts[0]}/></div>
-    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[1]}/></div>
-    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[2]}/></div>
-    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[3]}/></div>
-    <div class="col-md-4 col-lg-3"><Post data={this.props.posts[4]}/></div>
-    <div class="col-md-4 col-lg-6"><Post data={this.props.posts[5]}/></div>
-
-    </React.Fragment>
-    ;
+    let i = 0;
+    for(i=0; i< p_items.length; i++){
+      if(0 == i%6 || 5 == i%6){
+        display_posts.push(
+          <div className="col-md-4 col-lg-6" key={i}>
+                <Post data={ p_items[i]}/>
+            </div>
+        );
+      }
+      else{
+        display_posts.push(
+          <div className="col-md-4 col-lg-3" key={i}>
+            <Post data={ p_items[i]}/>
+          </div>
+        );
+      }
+    }
 
 
     return (
       <React.Fragment>
         <div class="row">
-          {all_posts}
+          {display_posts}
         </div>
       </React.Fragment>
     );
