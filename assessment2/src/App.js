b42f82883e91bcf46c2788b585851a518bb5c206
xmclallen <xmclallen@gmail.com>
2018-11-11 11:48:29 -0700

    Make a GET request via `fetch`

--- assessment2/src/App.js
+++ assessment2/src/App.js
@@ -7,6 +7,17 @@ import PostItems from './PostItems.js';
 
 class App extends Component {
   render() {
+
+    
+    //Make a fetch request -- defaults to GET
+    let listingsURL = "https://sv-reqres.now.sh/api/listings?per_page=9"
+    fetch(listingsURL)
+      .then( response => response.json()) //handle the data/response
+      .then( returned => console.log(JSON.stringify(returned))) //and do something with it
+      .catch( error => console.error('ERROR: ', error));
+    
+
+
     return (
       <div className="App">
         <div class="container">
